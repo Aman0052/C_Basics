bc6a496e3bba70db3fd9ddf64322dc66b32c8aea
Aman <aman.c9810@gmail.com>
2023-01-28 13:41:30 +0530

    Taking input from user in array using loop

---
+++ PrintEven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int printeven(int arr[], int n){
+    int count=0;
+    for( int i=0;i<n;i++){
+        if(arr[i]%2==0){
+            count++;
+        }
+    }
+    return count;
+}
+int main()
+{
+    int arr[]={1,2,3,4,5,6,7,8};
+    printf("%d ",printeven(arr,8));
+return 0;
+}
