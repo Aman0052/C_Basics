7d2e4bce8e293c670d0b1d08fdb7f6b9aaeb7282
Aman <aman.c9810@gmail.com>
2023-01-28 12:54:12 +0530

    Finding factorial of given number using pointer

---
+++ SumAverage.ptr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int sum(int *a,int *b){
+    int sum;
+    sum=*a+ *b;
+    return sum;
+}
+int average(int *a,int *b){
+    int avg;
+    avg=(*a+*b)/2;
+    return avg;
+}
+int main()
+{
+int a=10,b=20;
+printf("the value of sum is %d",sum(&a,&b));
+printf("the value of average is %d",average(&a,&b));
+return 0;
+}
