8927d8636972dec555989e6e14e6526457b885e7
Aman <aman.c9810@gmail.com>
2023-02-06 13:57:39 +0530

    Reallocate Function in Dynamic Memory Allocation

---
+++ ReallocFcn.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int *ptr;
+//printf("The size of int in my pc is %d",sizeof(int));
+ptr=(int*)malloc(6 * sizeof(int));
+for(int i=0;i<6;i++){
+    printf("Enter the value of %d element: ",i);
+    scanf("%d",&ptr[i]);
+}
+printf("\n");
+for(int i=0;i<6;i++){
+    printf("The value of %d element: %d\n",i,ptr[i]);
+}
+//REALLOCATE MEMORY
+ptr=realloc(ptr,10 * sizeof(int));
+for(int i=0;i<10;i++){
+    printf("Enter the value of %d element: ",i);
+    scanf("%d",&ptr[i]);
+}
+printf("\n");
+for(int i=0;i<10;i++){
+    printf("The value of %d element: %d\n",i,ptr[i]);
+}
+return 0;
+}
